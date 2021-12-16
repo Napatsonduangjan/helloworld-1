f77c37a0f4d3914d815f439f65396b5796158104
Thanabodee <thanabodee.s@kkumail.com>
2021-12-16 11:21:19 +0700

    add c to f

---
+++ TeampCalculator/TeampCalculator/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TeampCalculator
+{
+    partial class bouttonConvert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.textBoxF = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(88, 82);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(177, 26);
+            this.textBoxC.TabIndex = 0;
+            // 
+            // textBoxF
+            // 
+            this.textBoxF.Location = new System.Drawing.Point(88, 140);
+            this.textBoxF.Name = "textBoxF";
+            this.textBoxF.Size = new System.Drawing.Size(177, 26);
+            this.textBoxF.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "C";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "F";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(438, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Convert1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(438, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Convert2";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // bouttonConvert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 244);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxF);
+            this.Controls.Add(this.textBoxC);
+            this.Name = "bouttonConvert";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.TextBox textBoxF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TeampCalculator/TeampCalculator/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeampCalculator
+{
+    public partial class bouttonConvert : Form
+    {
+        public bouttonConvert()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+             //input
+            //นี้คือคอมเม้นท์
+            //textBoxC.Text = "hello";
+            //textBoxF.Text = "world";
+            //get input form textbox
+            string input = textBoxC.Text;
+            //convert to f 
+            // f= c* 9/5 +32
+            double c = Convert.ToDouble(input); 
+            double f = c * 9 / 5 + 32;
+            //show input to textbox
+            textBoxF.Text = f.ToString();
+        }
+    }
+}
